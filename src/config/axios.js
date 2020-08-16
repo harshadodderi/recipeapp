60ec38cf66729dcdb30c5046c2e92f6e475cd302
imnotharsha <harshadodderi@gmail.com>
2020-08-16 19:57:35 +0530

    cors error habdler link

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,3 +5,5 @@ const axios = Axios.create({
     'https://cors-anywhere.herokuapp.com/http://starlord.hackerearth.com/',
 });
 export default axios;
+
+// Added https://cors-anywhere.herokuapp.com/ this link to avoid CORS error
